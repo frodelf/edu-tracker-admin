412374aadf0b7a631295f5658c2949740d2d9279
Denis Derkach <derkach.denis@lll.kpi.ua>
2024-09-10 17:51:06 +0300

    Refactoring. Test CI/CD

--- src/main/resources/static/js/global.js
+++ src/main/resources/static/js/global.js
@@ -1,4 +1,4 @@
-var fullContextPath = 'http://localhost:8081/edu-tracker/admin/';
+var fullContextPath = 'http://slj.demodev.cc:7655/edu-tracker/admin/'
 var messageForDelete = "Об'єкт успішно видалено"
 var messageForSave = "Об'єкт успішно збережено"
 var pageSize = 10
