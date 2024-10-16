a5bbb895f721c80b7be52b6e83b427572530a6a0
Denis Derkach <derkach.denis@lll.kpi.ua>
2024-10-16 22:17:43 +0300

    ELK stack start

--- src/main/java/ua/kpi/edutrackeradmin/controller/MainController.java
+++ src/main/java/ua/kpi/edutrackeradmin/controller/MainController.java
@@ -1,13 +1,22 @@
 package ua.kpi.edutrackeradmin.controller;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
+import ua.kpi.edutrackeradmin.elasticsearch.Reindex;
 
 @Controller
+@RequiredArgsConstructor
 public class MainController {
+    private final Reindex reindex;
     @GetMapping({"/", ""})
     public ModelAndView main() {
+        return new ModelAndView("redirect:/course");
+    }
+    @GetMapping("/reindex")
+    public ModelAndView reindex() {
+        reindex.reindex();
         return new ModelAndView("redirect:/course");
     }
 }
